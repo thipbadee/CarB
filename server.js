f052b4a46df7f6134dd840a1259f9ea6aa7c5834
Amphi <6633287021@student.chula.ac.th>
2024-03-26 21:01:54 +0700

    deploy

--- server.js
+++ server.js
@@ -56,7 +56,13 @@ app.use('/api/v1/bookings', bookings);
 
     
 const PORT = process.env.PORT || 5000;
-const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
+const server = app.listen(
+    PORT, 
+    console.log(
+        'Server running in ', 
+        process.env.NODE_ENV, 
+        ' on '+ process.env.HOST + ':' + PORT
+    ));
 
 //Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise)=>{
